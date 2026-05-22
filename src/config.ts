d093077bc72410fd839c17c003df6a4e10cf6fa6
KeLLy1378 <59735563+KeLLy1378@users.noreply.github.com>
2026-05-22 21:41:26 +0500

    Change GROQ_API to a placeholder
    Updated GROQ_API constant to a placeholder value.

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const GROQ_API = "###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###";
+export const GROQ_API = "######";
 import type { Level } from "./types";
 
 export const PROMTS: Record<Level, string> = { // record создаёт объект с ключами типа Level и значениями типа string
